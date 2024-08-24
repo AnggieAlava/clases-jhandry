7434a0b6af099ee50e19e405eb5f73c2bca2ebee
Anggie Alava <codewithangels@gmail.com>
2024-08-24 16:03:59 -0500

    Contrasena ejercicio refactorizado

--- ejercicios/generador.py
+++ ejercicios/generador.py
@@ -1,32 +1,32 @@
 import random
 
 # definir tres variables que contendran los caracteres que queremos en nuestra contrasena 
-simbolos = ['!', '@'] # si es tipo 1 escogemos simbolos
-numeros = ['0', '1', '2'] # si es tipo 2 escogemos numero
-letras = ['a', 'e', 'i', 'o', 'u']  # <= Iterare sobre este conjunto para extraer cada caracter 
+uno = ['!', '@'] # si es tipo 1 escogemos simbolos
+dos = ['0', '1', '2'] # si es tipo 2 escogemos numero
+tres = ['a', 'e', 'i', 'o', 'u']  # <= Iterare sobre este conjunto para extraer cada caracter 
 
 # Iterar = Recorrer y obtener informacion de un conjunto de datos 
 
+def generar_contrasena(longitud): # Declarar una funcion => Usa la palabra reservada "def"
+    contrasena = "" # declaro una variable vacia aqui guardaremos la combinacion de los caracteres (total 6)
 
-def generar_contrasena(): # Declarar una funcion => Usa la palabra reservada "def"
-    contrasena = "" # aqui guardaremos la combinacion de los caracteres (total 6)
-
-
-    for dato in range(6): # seran 6 caracteres en cada contrasena 
+    for dato in range(longitud): # itero la longitud sera definida por el usuario
 
         tipo = random.randint(1,3) #1 es simbolos, 2 va a ser numeros, y 3 va a ser letras
 
         if tipo == 1:
-            contrasena  += random.choice(simbolos)
+            contrasena  += random.choice(uno)
         elif tipo == 2:
-            contrasena += random.choice(numeros)
+            contrasena += random.choice(dos)
         elif tipo == 3:
-            contrasena += random.choice(letras)
+            contrasena += random.choice(tres)
 
     return contrasena
 
 
-print(generar_contrasena())
+longitud = int(input("Cuantos caracteres quieres que tenga la contrasena? "))
+
+print("Tu contrasena es la siguiente:", generar_contrasena(longitud))
 
 
 
