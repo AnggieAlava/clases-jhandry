48878c1f5d5df1fa374ddae4c7d48c394a51a891
Anggie Alava <codewithangels@gmail.com>
2024-11-28 14:21:06 -0500

    Agregado ejercicio calculadora de cambio

---
+++ ejercicios/calculadora-cambio.py
@@ -0,0 +1,4 @@
+income = float(input('Ingresa cantidad entregada por el cliente: '))
+product_price = float(input('Ingresa el costo del porducto: '))
+cambio = income - product_price
+print(f"El cambio a entregar es de: {cambio:.2f}")
